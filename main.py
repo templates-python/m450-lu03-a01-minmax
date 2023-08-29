773493b6af6d37074d70440732a7ee90ca96a82f
Marcel Suter <marcel.suter@bzz.ch>
2023-08-29 10:39:15 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+import random
+
+
+def user_input():
+    numbers = list()
+    while True:
+        number = int(input('Zahl (0=Ende)? '))
+        if number == 0:
+            break
+        numbers.append(number)
+    min_max(numbers)
+    return numbers
+
+
+def random_numbers():
+    numbers = random.sample(range(-999, 999), 15)
+    min_max(numbers)
+    return numbers
+
+
+def min_max(numbers):
+    smallest = 2147483647
+    biggest = -2147483648
+    for number in numbers:
+        if number < smallest:
+            smallest = number
+        if number > biggest:
+            biggest = number
+    print(smallest)
+    print(biggest)
 
 
 if __name__ == '__main__':
-    calculate()
+    user_input()
+    random_numbers()
--- main_test.py
+++ main_test.py
@@ -1,6 +1,24 @@
-import main
+def test_random():
+    numbers = list()
+    # TODO: Rufe die Funktion "random_numbers" auf, speichere den Returnwert in einer Variable
+    # TODO: Übernimm die Ausgaben der Funktion
+    # TODO: Überprüfe ob die Ausgaben korrekt sind
+    print(f'{min(numbers)}\n{max(numbers)}\n')  # Diese Zeile ist als Hilfestellung gedacht
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+
+def test_userinput_normal(capsys, monkeypatch):
+    # TODO: Erzeuge eine Liste mit mehreren Inputwerten
+    # TODO: Lege die erzeugte Liste als "Benutzereingaben" fest
+    # TODO: Führe die Funktion "user_input" aus
+    # TODO: Übernimm die Ausgaben der Funktion
+    # TODO: Überprüfe das Resultat
+    pass
+
+
+def test_userinput_special(capsys, monkeypatch):
+    # TODO: Erzeuge eine Liste mit nur einem Inputwert
+    # TODO: Lege die erzeugte Liste als "Benutzereingaben" fest
+    # TODO: Führe die Funktion "user_input" aus
+    # TODO: Übernimm die Ausgaben der Funktion
+    # TODO: Überprüfe ob die Ausgaben korrekt sind
+    pass
